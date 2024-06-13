b6ebcbaa19acb6d819b49947b81fceeabc5939a9
Ceyda Sahin <ceydasahin889@gmail.com>
2024-06-13 23:49:25 +0300

    Init Vue project

---
+++ config-console-frontend/src/components/SigninButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <button class="styled-button">{{ msg }}</button>
+</template>
+
+<style scoped>
+.styled-button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+</style>
---
+++ config-console-frontend/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <TheWelcome />
+    <SigninButton msg="Hello" />
+  </main>
+</template>
+
+<script>
+import TheWelcome from '../components/TheWelcome.vue'
+import SigninButton from '../components/SigninButton.vue'
+
+export default {
+  components: {
+    TheWelcome,
+    SigninButton
+  }
+}
+</script>
