611d20999b5c73ef37e855ae29e1875b9a7f391c
Ceyda Sahin <ceydasahin889@gmail.com>
2024-06-16 21:00:29 +0300

    Add parameter value tags
    Added UI changes to create and manage audiences by country (tag).

--- config-console-frontend/src/components/ConfigurationTable.vue
+++ config-console-frontend/src/components/ConfigurationTable.vue
@@ -15,55 +15,92 @@ const closeModal = () => {
 const parameters = ref([
   {
     key: 'min_version',
-    value: '1.4.4',
+    value: [{ value_tag: 'default', value: '1.4.4' }],
     description: 'Minimum required version of the app',
     createDate: '10/05/2021 01:58'
   },
   {
     key: 'latest_version',
-    value: '1.4.7',
+    value: [{ value_tag: 'default', value: '1.4.7' }],
     description: 'Latest version of the app',
     createDate: '10/05/2021 01:58'
   },
   {
     key: 'pricing_tier',
-    value: 't6',
+    value: [
+      { value_tag: 'default', value: '16' },
+      { value_tag: 'TR', value: '20' },
+      { value_tag: 'US', value: '10' },
+      { value_tag: 'DEU', value: '20' },
+      { value_tag: 'FR', value: '20' },
+      { value_tag: 'TR', value: '20' },
+      { value_tag: 'BR', value: '19' },
+      { value_tag: 'NL', value: '13' },
+      { value_tag: 'DEN', value: '11' }
+    ],
     description: 'Pricing tier of the user',
     createDate: '07/07/2021 11:13'
   },
   {
     key: 'scroll',
-    value: '5',
+    value: [{ value_tag: 'default', value: '5' }],
     description: 'Index of Scroll Paywall for free users.',
     createDate: '25/08/2021 10:22'
   },
   {
     key: 'scroll_limit',
-    value: '13',
+    value: [{ value_tag: 'default', value: '13' }],
     description: 'Index of Scroll Limit Paywall for free users.',
     createDate: '25/08/2021 10:23'
   }
 ])
 
-const newParameter = ref({ key: '', value: '', description: '', createDate: '' })
+const newParameter = ref({
+  key: '',
+  value: [{ value_tag: '', value: '' }],
+  description: '',
+  createDate: ''
+})
 
 const addParameter = () => {
-  if (newParameter.value.key && newParameter.value.value && newParameter.value.description) {
+  if (
+    newParameter.value.key &&
+    newParameter.value.value[0].value &&
+    newParameter.value.description
+  ) {
     newParameter.value.createDate = getCurrentTime()
+    if (newParameter.value.value[0].value_tag == '') {
+      newParameter.value.value[0].value_tag = 'default'
+    }
     parameters.value.push({ ...newParameter.value })
-    newParameter.value = { key: '', value: '', description: '', createDate: '' }
-  }
+    newParameter.value = {
+      key: '',
+      value: [{ value_tag: '', value: '' }],
+      description: '',
+      createDate: ''
+    }
 
-  //todo: send backend api post request
+    //todo: send backend api post request
+  }
 }
 
-const editParameterData = ref({ key: '', value: '', description: '', createDate: '' })
+const editIndex = ref(-1)
+
+const editParameterData = ref({
+  key: '',
+  value: [{ value_tag: '', value: '' }],
+  description: '',
+  createDate: ''
+})
 
 const editParameter = (index: number) => {
   editParameterData.value = { ...parameters.value[index] }
+  editIndex.value = -1
 }
 const saveParameter = (index: number) => {
   parameters.value[index] = { ...editParameterData.value }
+  editIndex.value = -1
+  // todo
 }
 
 const deleteParameter = (index: number) => {
@@ -87,71 +124,84 @@ function getCurrentTime(): string {
 </script>
 
 <template>
-  <div class="table-container">
-    <div class="table-header">Parameter Key</div>
-    <div class="table-header">Value</div>
-    <div class="table-header">Description</div>
-    <div class="table-header">Create Date</div>
-    <div class="table-header">Actions</div>
-    <div v-for="(parameter, index) in parameters" :key="index" class="table-row">
-      <div class="table-item">{{ parameter.key }}</div>
-      <div class="table-item">{{ parameter.value }}</div>
-      <div class="table-item">{{ parameter.description }}</div>
-      <div class="table-item">{{ parameter.createDate }}</div>
-      <div class="table-item">
-        <button @click="showModal" class="edit-btn">Edit</button>
-        <CustomModal v-if="isModalActive" @close="closeModal" />
-        <button @click="deleteParameter(index)" class="delete-btn">Delete</button>
+  <div class="config-page-container">
+    <div class="table-container">
+      <div class="table-header">Parameter Key</div>
+      <div class="table-header">Value</div>
+      <div class="table-header">Description</div>
+      <div class="table-header">Create Date</div>
+      <div class="table-header">Actions</div>
+      <div v-for="(parameter, index) in parameters" :key="index" class="table-row">
+        <div class="table-item">{{ parameter.key }}</div>
+        <div class="table-item">
+          <div class="values" v-for="val in parameter.value" :key="val.value_tag">
+            <div class="value-tag">{{ val.value_tag }}</div>
+            <div class="value-value">{{ val.value }}</div>
+          </div>
+        </div>
+        <div class="table-item">{{ parameter.description }}</div>
+        <div class="table-item">{{ parameter.createDate }}</div>
+        <div class="table-item">
+          <button @click="showModal" class="edit-btn">Edit</button>
+          <CustomModal v-if="isModalActive" @close="closeModal" />
+          <button @click="deleteParameter(index)" class="delete-btn">Delete</button>
+        </div>
       </div>
-    </div>
 
-    <div class="parameter-key-input">
-      <input v-model="newParameter.key" placeholder="New Parameter" />
-    </div>
-    <div class="parameter-value-input">
-      <input v-model="newParameter.value" placeholder="Value" />
-    </div>
-    <div class="parameter-description-input">
-      <input v-model="newParameter.description" placeholder="New Description" />
-    </div>
-    <button @click="addParameter" class="add-btn">Add</button>
-  </div>
-
-  <div class="card-container">
-    <div class="card" v-for="(parameter, index) in parameters" :key="index">
-      <div class="card-header">
-        <div class="parameter-group">
-          <h2><b>Parameter Key: </b>{{ parameter.key }}</h2>
-        </div>
-        <div class="parameter-group">
-          <h2><b>Value: </b>{{ parameter.value }}</h2>
-        </div>
-        <div class="parameter-group">
-          <h2> <b>Description: </b> {{ parameter.description }}</h2>
-        </div>
-
-        <div class="parameter-group">
-          <h2><b>Create Date: </b>{{ parameter.createDate }}</h2>
-        </div>
+      <div class="parameter-key-input">
+        <input v-model="newParameter.key" placeholder="New Parameter" />
       </div>
-      <div class="card-actions">
-        <button @click="editParameter(index)" class="edit-btn">Edit</button>
-        <button @click="deleteParameter(index)" class="delete-btn">Delete</button>
+      <div class="parameter-value-input">
+        <input v-model="newParameter.value[0].value" placeholder="Value" />
+        <input v-model="newParameter.value[0].value_tag" placeholder="Tag" />
       </div>
-      <div v-if="editIndex === index" class="edit-form">
-        <input v-model="editParameterData.key" placeholder="Edit Parameter Key" />
-        <input v-model="editParameterData.value" placeholder="Edit Value" />
-        <input v-model="editParameterData.description" placeholder="Edit Description" />
-        <input v-model="editParameterData.createDate" placeholder="Edit Create Date" />
-        <button @click="saveParameter(index)" class="save-btn">Save</button>
-        <button @click="cancelEdit" class="cancel-btn">Cancel</button>
+      <div class="parameter-description-input">
+        <input v-model="newParameter.description" placeholder="New Description" />
       </div>
-    </div>
-    <div class="card add-card">
-      <input v-model="newParameter.key" placeholder="New Parameter" />
-      <input v-model="newParameter.value" placeholder="Value" />
-      <input v-model="newParameter.description" placeholder="New Description" />
       <button @click="addParameter" class="add-btn">Add</button>
+    </div>
+
+    <div class="card-container">
+      <div class="card" v-for="(parameter, index) in parameters" :key="index">
+        <div class="card-header">
+          <div class="parameter-group">
+            <h2><b>Parameter Key: </b>{{ parameter.key }}</h2>
+          </div>
+          <div class="parameter-group">
+            <h2><b>Value: </b></h2>
+            <div class="values" v-for="val in parameter.value" :key="val.value_tag">
+              <div class="value-tag">{{ val.value_tag }}</div>
+              <div class="value-value">{{ val.value }}</div>
+            </div>
+          </div>
+          <div class="parameter-group">
+            <h2><b>Description: </b> {{ parameter.description }}</h2>
+          </div>
+
+          <div class="parameter-group">
+            <h2><b>Create Date: </b>{{ parameter.createDate }}</h2>
+          </div>
+        </div>
+        <div class="card-actions">
+          <button @click="editParameter(index)" class="edit-btn">Edit</button>
+          <button @click="deleteParameter(index)" class="delete-btn">Delete</button>
+        </div>
+        <div v-if="editIndex === index" class="edit-form">
+          <input v-model="editParameterData.key" placeholder="Edit Parameter Key" />
+          <input v-model="editParameterData.value" placeholder="Edit Value" />
+          <input v-model="editParameterData.description" placeholder="Edit Description" />
+          <input v-model="editParameterData.createDate" placeholder="Edit Create Date" />
+          <button @click="saveParameter(index)" class="save-btn">Save</button>
+          <button class="cancel-btn">Cancel</button>
+        </div>
+      </div>
+      <div class="card add-card">
+        <input v-model="newParameter.key" placeholder="New Parameter" />
+        <input v-model="newParameter.value[0].value" placeholder="Value" />
+        <input v-model="newParameter.value[0].value_tag" placeholder="Tag" />
+        <input v-model="newParameter.description" placeholder="New Description" />
+        <button @click="addParameter" class="add-btn">Add</button>
+      </div>
     </div>
   </div>
 </template>
@@ -159,7 +209,8 @@ function getCurrentTime(): string {
 <style scoped>
 .table-container {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr auto;
+  grid-template-rows: 1fr;
   gap: 40px;
   align-items: center;
   margin-top: 5vh;
@@ -175,6 +226,8 @@ function getCurrentTime(): string {
 }
 
 .table-item {
+  width: fit-content;
+
   font-weight: 300;
   font-size: 14px;
   color: var(--color-p);
@@ -207,34 +260,69 @@ function getCurrentTime(): string {
   grid-column: 5;
 }
 
+.values {
+  display: inline-flex;
+  align-items: center;
+  margin-right: 10px;
+  margin-bottom: 5px;
+  gap: 2px;
+}
+
+.value-value {
+  display: inline-flex;
+}
+
+.value-tag {
+  cursor: pointer;
+  font-weight: 400;
+  font-size: 10px;
+  display: inline-flex;
+  padding: 5px;
+  border-radius: 5px;
+  background: var(--gradient-tag);
+}
+
 input {
   padding: 10px;
   border: 1px solid #2d3551;
   border-radius: 8px;
+
   background-color: transparent;
   color: white;
   font-size: 14px;
-  width: 90%;
 }
 input:focus {
-  border: 1px solid var(--color-border); /* Example border color */
-  outline: none; /* Remove default outline */
-  box-shadow: none; /* Remove any box-shadow */
+  border: 1px solid var(--color-border);
+  outline: none;
+  box-shadow: none;
 }
 
 .parameter-key-input {
+  width: fit-content;
   grid-column: 1;
+}
+.parameter-key-input input {
   align-content: start;
-  width: 90%;
 }
 
 .parameter-value-input {
+  width: fit-content;
   grid-column: 2;
-  width: 90%;
+}
+
+.parameter-value-input input {
+  align-content: start;
+  width: 5ch;
+  margin-right: 10px;
 }
 
 .parameter-description-input {
-  grid-column: 3/5;
+  grid-column: 3 / 5;
+  width: 90%;
+}
+
+.parameter-description-input input {
+  width: 100%;
 }
 
 .card-container {
@@ -254,7 +342,9 @@ input:focus {
 
   .parameter-group {
     display: flex;
-    align-items: start;
+    align-items: center;
+    flex-wrap: wrap;
+    text-align: center;
     align-content: center;
     gap: 0.3rem;
     margin-bottom: 0.9rem;
@@ -272,8 +362,12 @@ input:focus {
     justify-content: space-between;
   }
 
+  .value-value {
+    font-size: 15px;
+    font-weight: 300;
+  }
   b {
-    font-weight:500;
+    font-weight: 500;
   }
   h2 {
     font-size: 15px;
